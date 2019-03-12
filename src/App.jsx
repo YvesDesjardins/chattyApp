c5a50cdfeef412ce24afee765a474e19680d3d5f
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-12 11:37:38 -0700

    set initial state along with test content

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: { name: 'Bob' },
+      messages: [{
+        username: 'Bob',
+        content: 'Has anyone seen my marbles?',
+      },
+      {
+        username: 'Anonymous',
+        content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+      }],
+    };
+  }
   render() {
     return (
       <fragment>
