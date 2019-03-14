3846df4c473f83314db6c7ff0ee5371e93b80a84
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-14 15:07:00 -0700

    implement style change for notifications

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,10 +4,15 @@ import React from 'react';
 // parse message to check if gif or not
 export default function Message(props) {
   function parseMessage() {
-    if (props.type === 'gifMessage') {
-      return <div className='message-content'><img src={props.content} alt='Gif' /></div>
-    } else {
-      return <div className='message-content'>{props.content}</div>;
+    switch (props.type) {
+      case 'gifMessage':
+        return <div className='message-content'><img src={props.content} alt='Gif' /></div>;
+      case 'notificationMessage':
+      case 'errorMessage':
+        return <div className='message-content'><div className='highlight'>{props.content}</div></div>;
+      default:
+        return <div className='message-content'>{props.content}</div>;
+
     }
   }
 
