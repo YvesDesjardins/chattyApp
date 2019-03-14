0b5611187aba713b7b6c27062abc00a558dd81d9
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-14 11:01:01 -0700

    update to use broadcast function to message all clients

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,7 +1,7 @@
 // server.js
 
 const express = require('express');
-const SocketServer = require('ws').Server;
+const SocketServer = require('ws');
 const uuid = require('uuid/v4');
 
 // Set the port to 3001
@@ -14,7 +14,7 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
-const wss = new SocketServer({
+const wss = new SocketServer.Server({
   server
 });
 
@@ -23,15 +23,27 @@ const wss = new SocketServer({
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  console.log(`Connected clients: ${wss.clients.size}`);
 
   ws.on('message', (data) => {
     const message = JSON.parse(data);
     message.id = uuid();
 
-    console.log('Sent data!')
+    console.log('Sent data!');
     console.log(`User ${message.username} said ${message.content} with ID ${message.id}`);
-    wss.clients.forEach(client => client.send(JSON.stringify(message)));
+    wss.broadcast(JSON.stringify(message));
   });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    console.log(`Connected clients: ${wss.clients.size}`);
+  });
 });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === SocketServer.OPEN) {
+      client.send(data);
+    }
+  });
+};
